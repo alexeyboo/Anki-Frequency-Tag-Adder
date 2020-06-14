437b52714f86e55095577efcee17ab6d8f887dff
Alexey Bondarev <alexeybondarev45@gmail.com>
2020-06-14 20:01:45 +0300

    switched from word's family to individual word occurrence frequency

--- src/main/java/TxtUtils.java
+++ src/main/java/TxtUtils.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class TxtUtils {
-	public static Set<String[]> getSetOfEnAndTags(String fileName) {
+	public static Set<String[]> getSetOfNotes(String fileName) {
 		BufferedReader bufferedReader = IOUtils.getFileReader(fileName);
 		Set<String[]> setOfNotes = new HashSet<>();
 
@@ -32,11 +32,14 @@ public class TxtUtils {
 		try {
 			List<String[]> strings = csvReader.readAll();
 			for (String[] string : strings) {
-				frequencys.put(string[1], Integer.valueOf(string[0]));
+				String s = string[2];
+				if (!s.isEmpty()) {
+					frequencys.put(string[1], Integer.valueOf(s));
+				} else {
+					frequencys.put(string[1], 0);
+				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (CsvException e) {
+		} catch (IOException | CsvException e) {
 			e.printStackTrace();
 		}
 
@@ -45,31 +48,39 @@ public class TxtUtils {
 	}
 
 	public static Map<String, String> getNotesWithTags(String sourceDictionaryName, String frequencyListName) {
-		Set<String[]> notes = getSetOfEnAndTags(sourceDictionaryName);
+		Set<String[]> notes = getSetOfNotes(sourceDictionaryName);
 		Map<String, Integer> frequency = getMapOfPositionAndEns(frequencyListName);
 		Map<String, String> toReturn = new HashMap<>();
 
 		for (String[] note : notes) {
-			int max = 0;
+			int min = 10000000;
 			String tagsToAppend = "";
 			String[] words = note[0].split(" ");
 			for (String word : words) {
 				if (word.contains("-")) {
 					String[] split = word.split("-");
 					for (String s : split) {
-						Integer i = frequency.get(s);
-						if (i != null && i > max) {
-							max = i;
-						}
+						min = getMin(frequency, min, s);
 					}
 				}
-				Integer i = frequency.get(word);
-				if (i != null && i > max) {
-					max = i;
+
+				if (word.contains("/")) {
+					String[] split = word.split("/");
+					for (String s : split) {
+						min = getMin(frequency, min, s);
+					}
 				}
+				min = getMin(frequency, min, word);
 			}
 
-			tagsToAppend += max + "k";
+			StringBuilder s = new StringBuilder(String.valueOf(min));
+
+			int length = s.length();
+			for (int i = 0; i < 7 - length; i++) {
+				s.insert(0,0);
+			}
+
+			tagsToAppend += s;
 
 			String[] tags = note[1].split(" ");
 			for (String tag : tags) {
@@ -82,5 +93,15 @@ public class TxtUtils {
 		}
 
 		return toReturn;
+	}
+
+	public static int getMin(Map<String, Integer> frequency, int min, String word) {
+		Integer i = frequency.get(word);
+		if (i != null && i < min) {
+			min = i;
+		} else if (i == null) {
+			return 0;
+		}
+		return min;
 	}
 }
